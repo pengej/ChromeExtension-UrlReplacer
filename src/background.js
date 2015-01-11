f07ee836e1f01e9a845db7b66607b22adce529ae
PengEJ <pengej@163.com>
2015-01-11 22:47:52 +0800

    增加对http header设置的功能

--- src/background.js
+++ src/background.js
@@ -14,6 +14,7 @@ key是正则表达式
 , "\\.360\\.com":"[cancel]"
 */
 var redirectMap = {};
+var headersMap = {};
 var mapCount = new Map();
 
 function getRedirectUrl(key, url) {
@@ -45,7 +46,10 @@ function isInWhiteList(url) {
 
 function showIcon(id, count) {
   if(count!=undefined) {
-    str = "本页有资源地址被替换"+count.redirected+"处，被阻止"+count.canceled+"处";
+    str = "本页处理资源地址：\n"+
+         " 替换\t\t"+count.redirected+"处\n"+
+         " 阻止\t\t"+count.canceled+"处\n"+
+         " 修改HTTP头\t"+count.header+"处";
   }else{
     str = "本页有资源地址被替换或被阻止";
   }
@@ -61,7 +65,19 @@ function loadWhiteList(json) {
   whiteList = JSON.parse(json);
 }
 function loadRedirectMap(json) {
-  redirectMap = JSON.parse(json);
+   var temp = JSON.parse(json);
+   redirectMap = {};
+   headersMap = {};
+   for(var key in temp) {
+      var c = temp[key];
+      if((typeof c=='string')) {
+        redirectMap[key] = c;
+      } else if((typeof c=='object')) {
+        if(c["headers"]!=undefined) {
+          headersMap[key] = c["headers"];
+        }
+      }
+   }
 }
 function loadConfig(config) {
   loadWhiteList(config.whiteList);
@@ -92,52 +108,87 @@ chrome.webRequest.onBeforeRequest.addListener(
     if(isInWhiteList(info.url))
       return;
 
+    var count = mapCount.get("id"+info.tabId);
+    if(count==undefined) {
+      count = {
+        redirected : 0,
+        canceled : 0,
+        header : 0
+      };
+      mapCount.set("id"+info.tabId, count);
+    }
 
-
-    chrome.tabs.query({'active': true, 'currentWindow':true}, function(tabs) {
-
-      var count = mapCount.get("id"+tabs[0].id);
-      if(count==undefined) {
-        count = {
-          redirected : 0,
-          canceled : 0
-        };
-        mapCount.set("id"+tabs[0].id, count);
-      }
-
-      for(var key in redirectMap) {
-        var reg = new RegExp(key);
-        if(info.url.match(reg)) {
-          var newUrl = getRedirectUrl(key, info.url);
-          
-          if("[cancel]"==newUrl) {
-            console.log("阻止加载 "+info.url);
-            count.canceled++;
-            showIcon(tabs[0].id, count);
-            return {cancel: true};
-          } else {
-            console.log("重定向 "+info.url+" 到 "+newUrl);
-            count.redirected++;
-            showIcon(tabs[0].id, count);
-            return {redirectUrl: newUrl};
-          }
+    for(var key in redirectMap) {
+      var reg = new RegExp(key);
+      if(info.url.match(reg)) {
+        var newUrl = getRedirectUrl(key, info.url);
+        
+        if("[cancel]"==newUrl) {
+          console.log("阻止加载 "+info.url);
+          count.canceled++;
+          showIcon(info.tabId, count);
+          return {cancel: true};
+        } else {
+          console.log("重定向 "+info.url+" 到 "+newUrl);
+          count.redirected++;
+          showIcon(info.tabId, count);
+          return {redirectUrl: newUrl};
         }
       }
-
-
-    });
-
-
+    }
   },
   // filters
   {
     urls: [
       "*://*/*"
-    ]/*,
-    types: ["stylesheet","script"]*/
+    ]
   },
   // extraInfoSpec
   ["blocking"]);
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+function(info) {
+  if(isInWhiteList(info.url))
+      return;
+  var count = mapCount.get("id"+info.tabId);
+  if(count==undefined) {
+    count = {
+      redirected : 0,
+      canceled : 0,
+      header : 0
+    };
+    mapCount.set("id"+info.tabId, count);
+  }
+
+  for(var key in headersMap) {
+    var reg = new RegExp(key);
+    if(info.url.match(reg)) {
+      headers = headersMap[key];
+
+      for(var x1=0;x1<headers.length;x1++) {
+        var headerAdded = false;
+        for(var x2=0;x2<info.requestHeaders.length;x2++) {
+          if(headers[x1].name==info.requestHeaders[x2].name) {
+            info.requestHeaders.splice(x2, 1, headers[x1]);
+            headerAdded = true;
+          }
+        }
+        if(!headerAdded) {
+          info.requestHeaders.concat(headers[x1]);
+        }
+      }
+
+      if(headers.length>0) {
+        count.header+=headers.length;
+        console.log("修改 "+info.url+" HTTP请求头 "+headers.length+" 处");
+        showIcon(info.tabId, count);
+      }
+    }
+  }
+  return {requestHeaders: info.requestHeaders};
+},
+{urls: ["<all_urls>"]},
+["blocking", "requestHeaders"]);
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
   if(changeInfo.status=='loading') {
@@ -153,7 +204,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
       count.canceled=0;
     }
 
-    chrome.pageAction.hide(id);
+    chrome.pageAction.hide(tabId);
   }  
 });
 
